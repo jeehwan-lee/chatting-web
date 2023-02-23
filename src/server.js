ad0c4fe2b52789480154c547bc93dc8b7418523a
jeehwan <tjtojan@naver.com>
2023-02-24 01:12:53 +0900

    html과 express 연동

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.use("/js", express.static(__dirname + "/js"));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/" + "home.html"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => {
   console.log("Server Start");
 };
