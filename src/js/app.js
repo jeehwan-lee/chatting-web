787a5d69d9593628ac8d413b9448b3cce37cba27
jeehwan <tjtojan@naver.com>
2023-02-24 18:13:34 +0900

    방 목록 보여주기

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,58 @@
 const socket = io();
 
-const form = document.querySelector("form");
+const form = document.getElementById("textSend");
+const room = document.getElementById("room");
+const roomList = document.getElementById("roomList");
+const roomMake = document.getElementById("roomMake");
+const modal = document.getElementById("modal");
+
+form.hidden = true;
+modal.hidden = true;
+
+socket.on("init", (rooms) => {
+  rooms.forEach((room) => {
+    const li = document.createElement("li");
+    li.textContent = room;
+    roomList.appendChild(li);
+  });
+});
+
+function makeRoom(event) {
+  const roomName = document.getElementById("roomNameText");
+  socket.emit("make_room", roomName.value);
+  // TODO : 동일한 채팅방 이름이 있을 경우 (추후 DB로 확인)
+
+  // TODO : 채팅방을 만들 경우 채팅방 이름 저장 (추후 DB에 저장)
+  event.preventDefault();
+  // 방 생성
+  // TODO : 추후에는 서버로 방 이름을 보내서 검증 후 서버에서 생성
+  const li = document.createElement("li");
+  li.textContent = roomName.value;
+  roomList.appendChild(li);
+
+  // form과 modal hidden = true
+  room.hidden = true;
+  form.hidden = false;
+  modal.hidden = true;
+}
 
 function messageSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("message", input.value);
+  const input = document.getElementById("messageText");
+  socket.emit("send_message", input.value);
   input.value = "";
 }
 
+function enterRoom(event) {
+  socket.emit("enter_room", event.target.textContent);
+  room.hidden = true;
+  form.hidden = false;
+}
+
 form.addEventListener("submit", messageSubmit);
+roomList.addEventListener("click", enterRoom);
+// 채팅방 생성 modal 보여주기
+roomMake.addEventListener("click", () => {
+  modal.hidden = false;
+});
+modal.addEventListener("submit", makeRoom);
--- src/server.js
+++ src/server.js
@@ -6,14 +6,37 @@ const app = express();
 const httpServer = http.createServer(app);
 const socketServer = SocketIO(httpServer);
 
+const sockets = [];
+const rooms = ["room1", "room2"];
+
 app.use("/js", express.static(__dirname + "/js"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/" + "home.html"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 socketServer.on("connection", (socket) => {
-  // console.log(socket);
-  socket.on("message", (message) => console.log(message));
+  sockets.push(socket);
+
+  // 서버와 클라이언트가 연결되면 rooms 배열을 클라이언트로 보냄
+  socket.emit("init", rooms);
+
+  socket.on("send_message", (message) => console.log(message));
+
+  socket.on("enter_room", (roomName) => {
+    socket.join(roomName);
+    console.log(socket.rooms);
+  });
+
+  socket.on("make_room", (roomName) => {
+    if (rooms.includes(roomName) == true) {
+      // 이미 방이 만들어져있을 경우
+    } else {
+      // 방이 새로 만들어질 경우
+      rooms.push(roomName);
+      socket.join(roomName);
+      console.log(socket.rooms);
+    }
+  });
 });
 
 const handleListen = () => {
